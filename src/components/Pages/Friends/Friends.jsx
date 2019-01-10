b9022f6c7c05104575a1adf740fccd68886df664
mohammad <khezri@outlook.com>
2019-01-09 18:46:40 -0600

    created pages in componnets

---
+++ src/components/Pages/Friends/Friends.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Friends.scss';
+
+class Friends extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <button type="submit" className="btn btn-primary">Edit Friends</button>
+      </div>
+    );
+  }
+}
+
+export default Friends;
+
+
---
+++ src/components/Pages/HolidayFriends/HolidayFriends.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './HolidayFriends.scss';
+
+class HolidayFriends extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h3>Edit Friend Page</h3>
+      </div>
+    );
+  }
+}
+
+export default HolidayFriends;
+
+
